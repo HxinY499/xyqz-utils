a5849d96f26c4dd6afa2651857d7a492fc21287c
hxiny <xinyu.he@hand-china.com>
2021-10-13 16:45:48 +0800

    [UPDATE]删除多余内容

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -4,29 +4,6 @@ import { connect } from "dva";
 function IndexPage(props) {
   return (
     <div>
-      <button onClick={() => props.history.push("/myfirstpage")}>
-        跳转页面
-      </button>
-      <button
-        onClick={() => props.dispatch({ type: "example/save", payload: [4] })}
-      >
-        添加[4]
-      </button>
-      <button
-        onClick={() => {
-          console.log(
-            props.dispatch({ type: "products/change", payload: "bbb" })
-          );
-        }}
-      >
-        改变products bbb
-      </button>
-      <button onClick={() => console.log(props.example.test1)}>
-        输出example的state
-      </button>
-      <button onClick={() => console.log(props.products.a)}>
-        输出products的state
-      </button>
       <h1>Yay! Welcome to dva!</h1>
       <div />
     </div>
